2bd3c2f9e8bb7b92514a43f02c326263d584959d
viveksinghxogene <vivek.singh@xogene.com>
2026-04-22 12:59:47 +0530

    test case passing implementatio

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,17 @@
+#django to check connectio to database
+from django.core.management.base import BaseCommand
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+class Command(BaseCommand):
+    def handle(self,*args,**options):
+        self.stdout.write('Waiting for the database')
+        db_up=False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up=True
+            except (Psycopg2Error,OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second.....')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available!!'))
